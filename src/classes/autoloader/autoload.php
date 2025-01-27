8239bf0ac1a6d981e01f5321995110c7df2d28ec
elyandreb <elyandreburet72@gmail.com>
2025-01-27 16:45:21 +0100

    maj autoload fonctionnel

--- src/classes/autoloader/autoload.php
+++ src/classes/autoloader/autoload.php
@@ -6,10 +6,10 @@ spl_autoload_register(static function(string $fqcn) {
     
     if ($segments[0] === 'classes') {
         // Si c'est dans le namespace Classes :
-        $path = __DIR__ . '/' . $className . '.php';
+        $path = __DIR__ . '/../' . $className . '.php';
     } elseif ($segments[0] === 'bd') {
         // Si c'est dans le namespace BD :
-        $path = __DIR__ . '/bd/' . $className . '.php';
+        $path = __DIR__ . '/../bd/' . $className . '.php';
     }
     
     require_once $path;
