3ae9faee7f0d1a432135a49b927ce6a1e2f84dbe
Loris-Gr <grandchamploris@gmail.com>
2025-02-03 16:21:12 +0100

    transformation de la bd en classe

--- src/classes/bd/bd.php
+++ src/classes/bd/bd.php
@@ -1,15 +1,21 @@
 <?php
-$host = 'db.mexkcuymbqnzbkixjugo.supabase.co'; // Hôte Supabase
-$db = 'postgres'; // Nom de la base de données
-$user = 'postgres'; // Utilisateur
-$pass = 'LtdLLEeV,lg'; // Mot de passe
-$port = '5432'; // Port
 
-try {
-    $dsn = "pgsql:host=$host;port=$port;dbname=$db";
-    $pdo = new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-    echo "Connexion réussie!";
-} catch (PDOException $e) {
-    echo "Erreur de connexion : " . $e->getMessage();
+class model_bd {
+    private $pdo;
+
+    public function __construct() {
+        $host = 'db.mexkcuymbqnzbkixjugo.supabase.co'; // Hôte Supabase
+        $db = 'postgres'; // Nom de la base de données
+        $user = 'postgres'; // Utilisateur
+        $pass = 'LtdLLEeV,lg'; // Mot de passe
+        $port = '5432'; // Port
+        try {
+            $dsn = "pgsql:host=$host;port=$port;dbname=$db";
+            $this->pdo = new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        } catch (PDOException $e) {
+            echo "Erreur de connexion : " . $e->getMessage();
+        }
+    }
 }
+
 ?>
