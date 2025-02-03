7c17aecd5142be43e50c28165a2df654009caf2f
Loris-Gr <grandchamploris@gmail.com>
2025-02-03 17:50:06 +0100

    ajout insertion données dans bd

--- src/classes/bd/bd.php
+++ src/classes/bd/bd.php
@@ -12,9 +12,53 @@ class model_bd {
         try {
             $dsn = "pgsql:host=$host;port=$port;dbname=$db";
             $this->pdo = new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-        } catch (PDOException $e) {
-            echo "Erreur de connexion : " . $e->getMessage();
+        
+    $data = json_decode(file_get_contents('../data/restaurants_orleans.json'), true);
+
+        foreach ($data as $item) {
+            $stmt = $this->pdo->prepare("
+                INSERT INTO RESTAURANT (
+                    siret, nom_res, commune, departement, region, coordonnees, lien_site, num_tel, horaires_ouvert
+                ) VALUES (:siret, :nom_res, :commune, :departement, :region, :coordonnees, :lien_site, :num_tel, :horaires_ouvert)
+                ON CONFLICT (siret) DO NOTHING
+            ");
+            $stmt->bindParam(':siret', $item['siret']);
+            $stmt->bindParam(':nom_res', $item['name']);
+            $stmt->bindParam(':commune', $item['com_nom']);
+            $stmt->bindParam(':departement', $item['departement']);
+            $stmt->bindParam(':region', $item['region']);
+            $stmt->bindParam(':coordonnees', "{$item['coordinates'][0]},{$item['coordinates'][1]}");
+            $stmt->bindParam(':lien_site', $item['website']);
+            $stmt->bindParam(':num_tel', $item['phone']);
+            $stmt->bindParam(':horaires_ouvert', $item['opening_hours']);
+            $stmt->execute();
+
+            if (!empty($item['cuisine'])) {
+                foreach ($item['cuisine'] as $cuisine) {
+                    $stmt = $this->pdo->prepare("
+                        INSERT INTO TYPE_CUISINE (nom_type) VALUES (:cuisine)
+                        ON CONFLICT (nom_type) DO NOTHING
+                    ");
+                    $stmt->bindParam(':cuisine', $cuisine);
+                    $stmt->execute();
+
+                    $stmt = $this->pdo->prepare("
+                    INSERT INTO ETRE (id_res, id_type)
+                    VALUES ((SELECT id_res FROM RESTAURANT WHERE siret = :siret), (SELECT id_type FROM TYPE_CUISINE WHERE nom_type = :cuisine))
+                    ON CONFLICT DO NOTHING
+                    ");
+                    $stmt->bindParam(':siret', $item['siret']);
+                    $stmt->bindParam(':cuisine', $cuisine);
+                    $stmt->execute();
+                }
+            }
         }
+
+        $this->pdo->commit();
+        } catch (PDOException $e) {
+            $this->pdo->rollback();
+            echo "Erreur : " . $e->getMessage();
+        }     
     }
 
     public function getBD():PDO {
