1c9f0a396d8693ffbe4553efaad9083a861f7d07
elyandreb <elyandreburet72@gmail.com>
2025-03-03 22:04:57 +0100

    creation formulaire connexion

---
+++ src/templates/login_form.php
@@ -0,0 +1,9 @@
+<form action="classes/login.php" method="POST">
+    <label for="email">Email :</label>
+    <input type="email" name="email" required>
+    
+    <label for="mdp">Mot de passe :</label>
+    <input type="password" name="mdp" required>
+    
+    <button type="submit">Se connecter</button>
+</form>
