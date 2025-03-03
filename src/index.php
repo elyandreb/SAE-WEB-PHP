72baad81bbb7b3aafc7782d2aa7db8fb36115e34
elyandreb <elyandreburet72@gmail.com>
2025-03-03 16:58:46 +0100

    ajout action login

--- src/index.php
+++ src/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/classes/autoloader/autoload.php'; // Charge l'autoload
+session_start(); // Démarre la session
 use classes\Provider;
 use classes\Controller;
 
@@ -15,6 +16,22 @@ try {
         header('Location: index.php');
         exit;
     }
+
+    // Gérer le login
+    if ($action === 'login') {
+        if (!empty($_POST['email'])) {
+            $_SESSION['email'] = htmlspecialchars($_POST['email']);
+            header('Location: index.php?action=home');
+            exit;
+        }
+    }
+
+    // Vérifier si l'utilisateur est connecté
+    if (!isset($_SESSION['email'])) {
+        require_once __DIR__ . '/templates/login.php';
+        exit;
+    }
+
     print_r($action);
     if ($action === 'home') {
         $restaurants = Provider::getRestaurants(fichier: 'restaurants_orleans');
