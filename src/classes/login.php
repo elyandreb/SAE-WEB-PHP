08d9ea3d69f697c7388ab6ca79aa6e11916f81bd
elyandreb <elyandreburet72@gmail.com>
2025-03-03 22:07:14 +0100

    connexion implementé

---
+++ src/classes/login.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+require_once 'bd/model_bd.php';
+use bd\model_bd;
+
+$db = new model_bd();
+$email = $_POST['email'] ?? '';
+$mdp = $_POST['mdp'] ?? '';
+
+if (!empty($email) && !empty($mdp)) {
+    $user = $db->loginUser($email, $mdp);
+    
+    if ($user) {
+        $_SESSION['user_id'] = $user['id_u'];
+        $_SESSION['user_role'] = $user['le_role'];
+        $_SESSION['user_name'] = $user['nom_u'];
+        
+        header('Location: ../index.php?action=home');
+        exit();
+    } else {
+        include_once '../templates/login_form.php';
+        echo "Email ou mot de passe incorrect.";
+    }
+} else {
+    echo "Veuillez remplir tous les champs.";
+}
+?>
