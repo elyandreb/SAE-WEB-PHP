ef0a01412791b5e0ac90908319ebb854483b90fd
Loris-Gr <grandchamploris@gmail.com>
2025-03-09 12:37:53 +0100

    ajout phrase de confirmation

--- src/loadDB.php
+++ src/loadDB.php
@@ -4,6 +4,7 @@ use classes\model\Model_bd;
 try {
     $db = Model_bd::getInstance();
     $db->init_resto_json();
+    echo 'Base de donnée créée avec succès';
 } catch (Exception $e) {
     echo 'Exception reçue : ',  $e->getMessage(), "\n";
 }
